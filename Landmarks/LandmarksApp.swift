16deb0543e05b25d71450fb5423ca73dfb64c1a7
sean <topkiss123@hotmail.com>
2021-10-28 10:15:18 +0800

    Add done project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,37 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sean on 2021/3/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        let mainWindow = WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+        #if os(macOS)
+        mainWindow.commands {
+            LandmarkCommands()
+        }
+        #else
+        mainWindow
+        #endif
+        
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+        
+        #if os(macOS)
+        Settings {
+            LandmarkSettings()
+        }
+        #endif
+    }
+}
---
+++ WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by Sean on 2021/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
